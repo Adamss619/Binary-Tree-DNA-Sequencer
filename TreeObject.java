5b7ccdea20bcb8052d5a702abfb6c2cf56372b8c
justincarpenter <jncwinner@gmail.com>
2018-12-06 18:52:23 -0700

    btreenode and object update

--- TreeObject.java
+++ TreeObject.java
@@ -1,45 +1,50 @@
-public class TreeObject {
 
-	private long key;
+
+public class TreeObject implements Comparable<TreeObject> {
+
 	private int degree;
+	private long data;
 
-	// constructor 
-	public TreeObject(long key, int degree) {
-		setKey(key);
-		setDegree(degree);
+	public TreeObject(long d, int degree) {
+		this.degree = degree;
+		data = d;
 	}
 
-	/**
-	 * Sets key
-	 * @param key Value to be set
-	 */
-	public void setKey(long key) {
-		this.key = key;
+	public TreeObject(long d) {
+		degree = 1;
+		data = d;
 	}
-	
-	/**
-	 * Sets degree
-	 * @param degree offSet to be set
-	 */
+
+	public void increaseDegree() {
+		degree++;
+	}
+
+	public int getDegree() {
+		return degree;
+	}
+
 	public void setDegree(int degree) {
-
 		this.degree = degree;
 	}
-	
-	/**
-	 * Get the key
-	 * @return Returns the key of an object
-	 */
-	public long getKey() {
-		return this.key;
-	}
-	
-	/**
-	 * Get the degree
-	 * @return Returns the degree of an object
-	 */
-	public int getDegree() {
-		return this.degree;
+
+	public long getData() {
+		return data;
 	}
 
+	public void setData(long data) {
+		this.data = data;
+	}
+
+	public int compareTo(TreeObject Tobj) {
+		if (data < Tobj.data)
+			return -1;
+		if (data > Tobj.data)
+			return 1;
+		else
+			return 0;
+	}
+
+	public String toString() {
+		return "Key: " + data + "Frequency: " + degree;
+	}
 }
